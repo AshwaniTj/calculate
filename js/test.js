c017cc3d4b54e0c051bf57d1f8cbf1647fb89533
AshwaniTj <ashwani.tejan56@gmail.com>
2021-05-12 18:12:19 +0530

    add_multiply

---
+++ js/test.js
@@ -0,0 +1,14 @@
+
+function myfun1(){
+    var a = document.getElementById("number1").Value; 
+    var b = document.getElementById("number2").value;
+
+    document.getElementById("result").innerHTML=(a+b);
+}
+
+function myfun2(){
+    var a = document.getElementById("number1").Value; 
+    var b = document.getElementById("number2").value;
+
+    document.getElementById("result").innerHTML=(a*b);
+}
