eaf82a527990d07d806790fc24a768fafc263a35
AshwaniTj <ashwani.tejan56@gmail.com>
2021-05-12 18:26:45 +0530

    chamge function

--- js/test.js
+++ js/test.js
@@ -1,13 +1,13 @@
 
 function myfun1(){
-    var a = document.getElementById("number1").Value; 
+    var a = document.getElementById("number1").value; 
     var b = document.getElementById("number2").value;
 
     document.getElementById("result").innerHTML=(a+b);
 }
 
 function myfun2(){
-    var a = document.getElementById("number1").Value; 
+    var a = document.getElementById("number1").value; 
     var b = document.getElementById("number2").value;
 
     document.getElementById("result").innerHTML=(a*b);
